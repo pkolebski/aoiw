45c497660193c15175ee9ebbafe1670314ffe868
Piotr Kolebski <piotrkolebski@gmail.com>
2021-02-09 17:59:48 +0100

    add fourier transform

---
+++ fourier_transform.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+
+def fourier_transform(path):
+    img = cv2.imread(path, 0)
+    f = np.fft.fft2(img)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20 * np.log(np.abs(fshift))
+
+    plt.subplot(121), plt.imshow(img, cmap='gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+fourier_transform('data.png')
